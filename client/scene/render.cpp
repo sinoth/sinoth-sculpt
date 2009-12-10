b3ea311ccef89c9a8d776edd5d9f1c86fc01216b
sinoth <sinoth@4472a590-8e90-11de-800e-5b313e0d2bf3>
2009-12-10 22:31:01 +0000

    x

--- client/scene/render.cpp
+++ client/scene/render.cpp
@@ -2,6 +2,50 @@
 #include <stdio.h>
 #include "scene.h"
 
+/*
+ void sinLookAt(const vec3f &eyePosition, const vec3f &center, const vec3f &upVector)
+ {
+    float forward[3], side[3], up[3];
+    float matrix2[16], resultMatrix[16];
+    //------------------
+    forward[0] = center3D[0] - eyePosition3D[0];
+    forward[1] = center3D[1] - eyePosition3D[1];
+    forward[2] = center3D[2] - eyePosition3D[2];
+    NormalizeVector(forward);
+    //------------------
+    //Side = forward x up
+    ComputeNormalOfPlane(side, forward, upVector3D);
+    NormalizeVector(side);
+    //------------------
+    //Recompute up as: up = side x forward
+    ComputeNormalOfPlane(up, side, forward);
+    //------------------
+    matrix2[0] = side[0];
+    matrix2[4] = side[1];
+    matrix2[8] = side[2];
+    matrix2[12] = 0.0;
+    //------------------
+    matrix2[1] = up[0];
+    matrix2[5] = up[1];
+    matrix2[9] = up[2];
+    matrix2[13] = 0.0;
+    //------------------
+    matrix2[2] = -forward[0];
+    matrix2[6] = -forward[1];
+    matrix2[10] = -forward[2];
+    matrix2[14] = 0.0;
+    //------------------
+    matrix2[3] = matrix2[7] = matrix2[11] = 0.0;
+    matrix2[15] = 1.0;
+    //------------------
+    MultiplyMatrices4by4OpenGL_FLOAT(resultMatrix, matrix, matrix2);
+    glhTranslatef2(resultMatrix, -eyePosition3D[0], -eyePosition3D[1], -eyePosition3D[2]);
+    //------------------
+    memcpy(matrix, resultMatrix, 16*sizeof(float));
+ }
+*/
+
+
 
 void scene::render() {
 
@@ -14,15 +58,14 @@ void scene::render() {
 
     //glDisable(GL_TEXTURE_2D);
 
-
-
-
+/*
+    glTranslatef(0,0,-10);
     glBegin(GL_TRIANGLES);
         glColor4f(1.0,0.0,0.0,1.0); glVertex3f(0,0,0);
         glColor4f(0.0,1.0,0.0,1.0); glVertex3f(0,1,0);
         glColor4f(0.0,0.0,1.0,1.0); glVertex3f(1,0,0);
     glEnd();
-
+*/
 
     //cube
     glBegin(GL_QUADS);
@@ -57,6 +100,49 @@ void scene::render() {
         glVertex3f( 0.5, 0.5, 0.5);
         glVertex3f( 0.5,-0.5, 0.5);
     glEnd();
+
+
+    //glTranslatef(mainCamera.p_position.x, mainCamera.p_position.y,  mainCamera.p_position.z);
+    //glMultMatrixf(mainCamera.af_Matrix_pos);
+
+
+
+    //cube
+    glBegin(GL_QUADS);
+        //top
+        glColor4f(1.0,0.0,0.0,1.0);
+        glVertex3f(-0.25, 0.25, 0.25);
+        glVertex3f(-0.25, 0.25,-0.25);
+        glVertex3f( 0.25, 0.25,-0.25);
+        glVertex3f( 0.25, 0.25, 0.25);
+        //bottom
+        glColor4f(0.0,1.0,0.0,1.0);
+        glVertex3f(-0.25,-0.25, 0.25);
+        glVertex3f(-0.25,-0.25,-0.25);
+        glVertex3f( 0.25,-0.25,-0.25);
+        glVertex3f( 0.25,-0.25, 0.25);
+    glEnd();
+    glBegin(GL_LINES);
+        //four connecting lines
+        glColor4f(0.0,0.0,1.0,1.0);
+        glVertex3f(-0.25, 0.25,-0.25);
+        glVertex3f(-0.25,-0.25,-0.25);
+
+        glColor4f(1.0,1.0,0.0,1.0);
+        glVertex3f(-0.25, 0.25, 0.25);
+        glVertex3f(-0.25,-0.25, 0.25);
+
+        glColor4f(0.0,0.0,1.0,1.0);
+        glVertex3f( 0.25, 0.25,-0.25);
+        glVertex3f( 0.25,-0.25,-0.25);
+
+        glColor4f(1.0,1.0,0.0,1.0);
+        glVertex3f( 0.25, 0.25, 0.25);
+        glVertex3f( 0.25,-0.25, 0.25);
+    glEnd();
+
+    //glTranslatef(-mainCamera.p_position.x, -mainCamera.p_position.y, -mainCamera.p_position.z);
+
 
     glEnableClientState(GL_VERTEX_ARRAY);
     glEnableClientState(GL_COLOR_ARRAY);
@@ -205,11 +291,11 @@ void scene::generateVA() {
     temp_q.createFromAxisAngle(1,0,0,0);
     mainCamera.arcSetRotation(temp_q);
 
-    //mainCamera.arcSetCenter(vec3f(piece_x_size*1.5,piece_y_size*1.5,piece_z_size*1.5));
-    //mainCamera.arcSetRadius(3*piece_x_size);
+    mainCamera.arcSetCenter(vec3f(piece_x_size*1.5,piece_y_size*1.5,piece_z_size*1.5));
+    mainCamera.arcSetRadius(3*piece_x_size);
 
-    mainCamera.arcSetCenter(vec3f(0,0,0));
-    mainCamera.arcSetRadius(4);
+    //mainCamera.arcSetCenter(vec3f(0,0,0));
+    //mainCamera.arcSetRadius(4);
 
     mainCamera.arcRecalculate();
 
